071ffe2389b610c25afae7bd37a06992997d775f
David Cusi <robodavcuqupe@gmail.com>
2020-08-18 19:44:08 -0400

    Probando this.props en componente Video

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import Video from './components/Video'
 function App() {
   return (
     <React.Fragment>
-      <Video></Video> 
-      <Video></Video>   
+      <Video
+        titulo="Tutorial para estudiantes"
+        enlace="https://www.youtube.com/embed/-bI0diefasA"
+      /> 
+      <Video
+        titulo="Tutorial para docentes"
+        enlace="https://www.youtube.com/embed/-bI0diefasA"
+      />   
+      <Video
+        titulo="Tutorial básico de ZOOM"
+        enlace="https://www.youtube.com/embed/-bI0diefasA"
+      />   
     </React.Fragment>
     
   );
--- src/components/Video.js
+++ src/components/Video.js
@@ -5,11 +5,11 @@ class Video extends React.Component{
     render() {
         return <div className="Video">
             <div className="Video__section-header">
-                <h3 className="Video__titulo--size-desktop">Tutorial para estudiantes</h3>
+                <h3 className="Video__titulo--size-desktop">{this.props.titulo}</h3>
             </div>
             <div className="Video__section-video">
                 <iframe className="Video__video--size-desktop" type="text/html"
-            src='https://www.youtube.com/embed/-bI0diefasA' frameborder="0" allowfullscreen="true"></iframe>
+            src={this.props.enlace} frameborder="0" allowfullscreen="true"></iframe>
             </div>
         </div>
     }
